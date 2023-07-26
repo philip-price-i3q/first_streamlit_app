87f4295ea05a629db28b58d8bd0807e0817fa175
philip-price-i3q <19774716+philip-price-i3q@users.noreply.github.com>
2023-07-26 14:08:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Curly Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free Range Egg')
