a8e3b7b36f402be400d0fa9bbe8400e68e72707f
philip-price-i3q <19774716+philip-price-i3q@users.noreply.github.com>
2023-08-17 20:58:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,4 +54,4 @@ streamlit.text(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('The user entered : ', add_my_fruit)
